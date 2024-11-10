0991d0ceea558a456f78477836edaeffc480d356
Akash Agrawal <agrawalakash82@gmail.com>
2024-11-10 13:53:44 +0530

    bg-zinc-600

--- src/App.jsx
+++ src/App.jsx
@@ -34,7 +34,7 @@ function App() {
   return (
     <>
     <TodoProvider value={{todos , addTodo , toggleTodo , updateTodo , deleteTodo}}>
-      <div className="bg-green-800 min-h-screen py-8">
+      <div className="bg-zinc-600 min-h-screen py-8">
                 <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
                     <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
                     <div className="mb-4">
